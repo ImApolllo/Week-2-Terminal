38e509028587f98163940af2b2fa13612b318ccd
Jackson Geiger <jackson.v.geiger@gmail.com>
2022-10-03 21:22:48 -0700

    Updated to only print number if current is not divisable by 3 opr 5.

--- FizzBuzz/FizzBuzz.js
+++ FizzBuzz/FizzBuzz.js
@@ -8,5 +8,8 @@ for (i = 1; i <= 100; i++){
     if (i % 5 == 0){
         output = output + "Buzz"
     }
+    if (i % 3 != 0 && i % 5 != 0){
+        output = output + i;
+    }
     console.log(output);
 }
