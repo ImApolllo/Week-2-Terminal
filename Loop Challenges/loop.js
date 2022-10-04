d27d969acaafcb183ea36f9b1aff777cb79f2693
Jackson Geiger <jackson.v.geiger@gmail.com>
2022-10-03 23:01:22 -0700

    initial loop file

---
+++ Loop Challenges/loop.js
@@ -0,0 +1,19 @@
+var sum = 0;
+
+// for (i = 1; i < 21; i++){
+//     if (i % 2 != 0){
+//         console.log(i)
+//     }
+// }
+// for (i = 100; i >= 0; i--){
+//     if (i % 3 == 0){
+//         console.log(i);
+//     }
+// }
+// for (i = 4; i > -4; i -= 1.5){
+//     console.log(i);
+// }
+for (i = 1; i < 101; i++){
+    sum = sum + i; 
+}
+console.log(sum);
